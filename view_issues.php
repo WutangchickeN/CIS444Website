4ef1c63b0499ff661257a6fc617f683bca5442b1
Brittany <britt.garcia123@gmail.com>
2019-11-26 15:59:58 -0800

    made changes to view_issues.php

--- view_issues.php
+++ view_issues.php
@@ -5,7 +5,9 @@ $user = "team_c";
 $password = "3gCLD+9D+Fx8";
 $database = "team_c";
 
-$connect = mysqli_connect($host, $user, $password, $database)
+$connect = mysqli_connect($host, $user, $password, $database);
+$query = "SELECT * FROM 'ROOM'";
+$result1 = mysqli_query($connect, $query);
 
 ?>
 
@@ -84,8 +86,14 @@ $(".fa-bandcamp").on("click", function(){
 			<div class="mapouter"><div class="gmap_canvas"><iframe width="600" height="500" id="gmap_canvas" src="https://maps.google.com/maps?q=Search%20Results%20333%20S%20Twin%20Oaks%20Valley%20Rd%2C%20San%20Marcos%2C%20CA%2092096&t=&z=15&ie=UTF8&iwloc=&output=embed" frameborder="0" scrolling="no" marginheight="0" marginwidth="0"></iframe><a href="https://www.embedgooglemap.net/blog/divi-discount-code-elegant-themes-coupon/"></a></div><style>.mapouter{position:relative;text-align:right;height:500px;width:600px;}.gmap_canvas {overflow:hidden;background:none!important;height:500px;width:600px;}</style></div>
 		</p>
 
+		<select>
+			<?php while($row1 = mysqli_fetch_array($result1)):;?>
+			<option><?php echo $row1[1]?></option>
+			<?php endwhile;?>
+		</select>
+
 		<p> <!-- Select the Building -->
-			Building 
+			Building test
 			<select name = "Building">
 				<option> Any </option>
 				<option> Academic Hall </option>
